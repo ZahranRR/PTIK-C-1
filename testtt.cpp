f16b0b439e1cc6c747d52819d5f827950c7423ac
ZahranRR <zahranrrosyadi@gmail.com>
2023-12-01 14:11:06 +0700

    menambahkan file testtt.cpp

---
+++ testtt.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int nilai;
+cout << "masukkan nilai anda : "; cin >> nilai;
+if (nilai > 75)
+cout<<"selamat anda lulus";
+else
+cout<<"maaf ! anda gagal";
+}
