75c516213a78676ed5cdb03ee2e69eefc0aff583
rilann boi <mohammadafiffahrilan@gmail.com>
2023-12-05 14:57:31 +0700

    Mengupload versi 2

--- crudACNew.cpp
+++ crudACNew.cpp
@@ -16,3 +16,30 @@ const int MAX_AC = 100;
 AirConditioner acDatabase[MAX_AC];
 int acCount = 0;
 
+// Fungsi untuk menambahkan data AC
+void addAC() {
+    if (acCount < MAX_AC) {
+        AirConditioner newAC;
+        newAC.id = acCount + 1;
+
+        cout << "Masukkan Brand AC: ";
+        cin.ignore();
+        cin.getline(newAC.brand, sizeof(newAC.brand));
+
+        cout << "Masukkan Tipe AC: ";
+        cin.getline(newAC.type, sizeof(newAC.type));
+
+        cout << "Masukkan Kapasitas AC (ton): ";
+        cin >> newAC.capacity;
+
+        cout << "Masukkan Harga AC: ";
+        cin >> newAC.price;
+
+        acDatabase[acCount] = newAC;
+        acCount++;
+
+        cout << "Data AC berhasil ditambahkan." << endl;
+    } else {
+        cout << "Database AC penuh, tidak dapat menambahkan data baru." << endl;
+    }
+}
